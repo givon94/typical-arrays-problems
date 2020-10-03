020ade4a72c5404b3e62b16ff90eee72d4996327
givon94 <alexhivoina@gmail.com>
2020-10-03 22:02:26 +0300

    feat: add functions

--- src/index.js
+++ src/index.js
@@ -1,12 +1,24 @@
 
 exports.min = function min (array) {
-  return 0;
+    if (array && array.length > 0 && Array.isArray(array)) {
+        return Math.min(...array);
+    } else {
+        return 0;
+    }
 }
 
 exports.max = function max (array) {
-  return 0;
+    if (array && array.length > 0 && Array.isArray(array)) {
+        return Math.max(...array);
+    } else {
+        return 0;
+    }
 }
 
 exports.avg = function avg (array) {
-  return 0;
+    if (array && array.length > 0 && Array.isArray(array)) {
+        return array.reduce((a, b) => (a + b)) / array.length;
+    } else {
+        return 0;
+    }
 }
